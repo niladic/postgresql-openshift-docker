1a036479580772f43fc99e7e2d65953d07f49544
Julien DAUPHANT <judauphant@gmail.com>
2018-11-21 18:39:27 +0100

    Change backup type

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -3,7 +3,7 @@
 # and https://gist.github.com/shaiton/e505608c0b3bc9bc5aac
 
 NOW="$(date +"%Y-%m-%d-%s")"
-FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.sql.gz"
+FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.gz"
 find $DATABASE_BACKUP_PATH -name $DATABASE_NAME.*backup* -type f -mtime +$RETENTION_DAYS -exec rm '{}' \;
-pg_dump $DATABASE_URL | gzip > $FILENAME
+pg_dump -Fc $DATABASE_URL | gzip > $FILENAME
 
