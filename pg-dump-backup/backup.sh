b7cd59feed0dec8c618c97e51aace360af96fe09
Julien DAUPHANT <judauphant@gmail.com>
2018-11-21 17:35:34 +0100

    Add timestamp

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -2,7 +2,7 @@
 # Inspired from https://gist.github.com/skyebook/3407646
 # and https://gist.github.com/shaiton/e505608c0b3bc9bc5aac
 
-NOW="$(date +"%Y-%m-%d")"
+NOW="$(date +"%Y-%m-%d-%s")"
 FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.sql.gz"
 find $DATABASE_BACKUP_PATH -name $DATABASE_NAME.*backup* -type f -mtime +$RETENTION_DAYS -exec rm '{}' \;
 pg_dump $DATABASE_URL | gzip > $FILENAME
