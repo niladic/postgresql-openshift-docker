3ea33a341792d44d35bd9f9104a0babc7a70f9ff
Lucien Pereira <lucienpereiramailbox+bitbucket@gmail.com>
2020-01-02 10:11:46 +0100

    [102] En temps que devops, je voudrais un backup sur autre hébergeur (par exemple sur un stockage type S3).

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -7,7 +7,13 @@ FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.gz"
 find $DATABASE_BACKUP_PATH -name $DATABASE_NAME.*backup* -type f -mtime +$RETENTION_DAYS -exec rm '{}' \;
 pg_dump -Fc $DATABASE_URL | gzip > $FILENAME
 
-curl -o ${RECIPIENT_PUBLIC_KEY_EMAIL}.asc ${RECIPIENT_PUBLIC_KEY_URL}
+if [ -z "${RECIPIENT_PUBLIC_KEY}" ]
+then
+      echo "RECIPIENT_PUBLIC_KEY is empty."
+      exit 1
+fi
+
+echo ${RECIPIENT_PUBLIC_KEY} > ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --no-tty --import ${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --batch --trust-model always --output "${FILENAME}.gpg" --recipient ${RECIPIENT_PUBLIC_KEY_EMAIL} --encrypt ${FILENAME}
 
