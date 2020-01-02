ed39200017b77773fee290c661fa910db8c599fe
Lucien Pereira <lucienpereiramailbox+bitbucket@gmail.com>
2020-01-02 15:04:17 +0100

    [102] En temps que devops, je voudrais un backup sur autre hébergeur (par exemple sur un stockage type S3).

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -2,8 +2,6 @@
 # Inspired from https://gist.github.com/skyebook/3407646
 # and https://gist.github.com/shaiton/e505608c0b3bc9bc5aac
 
-set -x
-
 NOW="$(date +"%Y-%m-%d-%s")"
 FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.gz"
 find $DATABASE_BACKUP_PATH -name $DATABASE_NAME.*backup* -type f -mtime +$RETENTION_DAYS -exec rm '{}' \;
@@ -15,13 +13,10 @@ then
       exit 1
 fi
 
-echo ${RECIPIENT_PUBLIC_KEY} | base64 --decode > ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
-cat ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
+echo ${RECIPIENT_PUBLIC_KEY_B64} | base64 --decode > ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --no-tty --import ${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --batch --trust-model always --output "${FILENAME}.gpg" --recipient ${RECIPIENT_PUBLIC_KEY_EMAIL} --encrypt ${FILENAME}
 
 aws s3 cp "${FILENAME}.gpg" s3://${AWS_BUCKET_NAME}
 
 rm "${FILENAME}.gpg"
-
-set +x
