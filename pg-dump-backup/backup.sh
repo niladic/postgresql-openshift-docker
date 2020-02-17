aaddb5a948dfa164f51530a5fdef3ef75883d726
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2020-02-17 22:51:18 +0100

    Add debug

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -16,6 +16,7 @@ then
 fi
 
 echo "${RECIPIENT_PUBLIC_KEY}" | gpg --no-tty --import
+set -x
 gpg --batch --trust-model always --output "${FILENAME}.gpg" --recipient ${RECIPIENT_PUBLIC_KEY_EMAIL} --encrypt ${FILENAME}
 
 s3cmd --host=${S3_HOSTNAME} --host-bucket=${S3_HOSTBUCKET} put "${FILENAME}.gpg" s3://${S3_BUCKET_NAME}
