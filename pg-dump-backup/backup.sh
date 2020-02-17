f39733e98da5910523aecf1bddadba6b53c2fa8f
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2020-02-17 19:54:57 +0100

    Remove base64

--- pg-dump-backup/backup.sh
+++ pg-dump-backup/backup.sh
@@ -2,18 +2,21 @@
 # Inspired from https://gist.github.com/skyebook/3407646
 # and https://gist.github.com/shaiton/e505608c0b3bc9bc5aac
 
+set -e 
+set -x
+
 NOW="$(date +"%Y-%m-%d-%s")"
 FILENAME="$DATABASE_BACKUP_PATH/$DATABASE_NAME.$NOW.backup.gz"
 find $DATABASE_BACKUP_PATH -name $DATABASE_NAME.*backup* -type f -mtime +$RETENTION_DAYS -exec rm '{}' \;
 pg_dump -Fc $DATABASE_URL | gzip > $FILENAME
 
-if [ -z "${RECIPIENT_PUBLIC_KEY_B64}" ]
+if [ -z "${RECIPIENT_PUBLIC_KEY}" ]
 then
-      echo "Do no upload backup : RECIPIENT_PUBLIC_KEY_B64 is empty."
+      echo "Do no upload backup : RECIPIENT_PUBLIC_KEY is empty."
       exit 0
 fi
 
-echo ${RECIPIENT_PUBLIC_KEY_B64} | base64 --decode > ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
+echo ${RECIPIENT_PUBLIC_KEY} > ./${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --no-tty --import ${RECIPIENT_PUBLIC_KEY_EMAIL}.asc
 gpg --batch --trust-model always --output "${FILENAME}.gpg" --recipient ${RECIPIENT_PUBLIC_KEY_EMAIL} --encrypt ${FILENAME}
 
